9697aa6ec5dfe2bd37a7ee5a79568e7dfdb28131
Ha Van H.N <ha.van@asiantech.vn>
2020-09-17 09:17:27 +0700

    Change UI and config slide

--- EateryTour/View/Controller/Home/HomeViewController.swift
+++ EateryTour/View/Controller/Home/HomeViewController.swift
@@ -8,97 +8,105 @@
 
 import UIKit
 
-enum SectionType {
-    case trending
-    case category
-}
-
 final class HomeViewController: ViewController {
-
+    
     // MARK: - IBOutlets
     @IBOutlet private weak var tableView: TableView!
     @IBOutlet private weak var collectionView: CollectionView!
-
+    @IBOutlet private weak var pageControl: UIPageControl!
+    
     // MARK: - Propeties
     private var viewModel = HomeViewModel()
-
+    private var slideToRight: Bool = true
+    
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         configTableView()
         customNavigationBar()
         configCollectionView()
+        configSlide()
     }
-
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         statusBarStyle = .lightContent
     }
-
+    
     // MARK: - Private functions
     private func configTableView() {
         tableView.dataSource = self
         tableView.delegate = self
         let trendingCell = UINib(nibName: "TrendingTableViewCell", bundle: Bundle.main)
         tableView.register(trendingCell, forCellReuseIdentifier: "trendingCell")
-        let categoryCell = UINib(nibName: "CategoryTableViewCell", bundle: Bundle.main)
-        tableView.register(categoryCell, forCellReuseIdentifier: "categoryCell")
-        let header = UINib(nibName: "CustomHeaderView", bundle: Bundle.main)
-        tableView.register(header, forHeaderFooterViewReuseIdentifier: "header")
+        tableView.sectionIndexBackgroundColor = UIColor.white
+        tableView.sectionIndexTrackingBackgroundColor = UIColor.white
     }
-
+    
     private func configCollectionView() {
         collectionView.dataSource = self
         collectionView.delegate = self
         let slideCell = UINib(nibName: "SliderCell", bundle: Bundle.main)
         collectionView.register(slideCell, forCellWithReuseIdentifier: "SliderCell")
     }
-
+    
     private func customNavigationBar() {
         title = "Eatery Tour"
         navigationController?.navigationBar.barTintColor = #colorLiteral(red: 0.1602264941, green: 0.4939214587, blue: 0.4291425645, alpha: 1)
         navigationController?.navigationBar.titleTextAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
     }
+    
+    private func configSlide() {
+        Timer.scheduledTimer(withTimeInterval: 3, repeats: true) { _ in
+            if self.slideToRight {
+                self.pageControl.currentPage += 1
+                self.collectionView.scrollToItem(at: IndexPath(item: self.pageControl.currentPage, section: 0), at: .right, animated: true)
+                if self.pageControl.currentPage == 4 {
+                    self.slideToRight = false
+                }
+            } else {
+                self.pageControl.currentPage -= 1
+                self.collectionView.scrollToItem(at: IndexPath(item: self.pageControl.currentPage, section: 0), at: .left, animated: true)
+                if self.pageControl.currentPage == 0 {
+                    self.slideToRight = true
+                }
+            }
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
 extension HomeViewController: UITableViewDataSource {
-
-    func numberOfSections(in tableView: UITableView) -> Int {
-        return 2
-    }
-
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 10
     }
-
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let sectionType = viewModel.getSectionType(section: indexPath.section)
-        switch sectionType {
-        case .trending:
-            guard let trendingCell = tableView.dequeueReusableCell(withIdentifier: "trendingCell", for: indexPath) as? TrendingTableViewCell else { return TableCell() }
-            return trendingCell
-        case .category:
-            guard let categoryCell = tableView.dequeueReusableCell(withIdentifier: "categoryCell", for: indexPath) as? CategoryTableViewCell else { return TableCell() }
-            return categoryCell
-        }
+        guard let trendingCell = tableView.dequeueReusableCell(withIdentifier: "trendingCell", for: indexPath) as? TrendingTableViewCell else { return TableCell() }
+        return trendingCell
     }
-
-    internal func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        guard let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header") as? CustomHeaderView else { return UIView() }
-        let type = viewModel.getSectionType(section: section)
-        header.viewModel = viewModel.getConfigSection(sectionType: type)
-        return header
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let uiView = UIView(frame: CGRect(x: 0, y: 0, width: tableView.bounds.width, height: 50))
+        let label = UILabel(frame: CGRect(x: 15, y: 20, width: tableView.bounds.width, height: 30))
+        let attributes: [NSAttributedString.Key: Any] = [
+            .foregroundColor: UIColor.black,
+            .font: UIFont.boldSystemFont(ofSize: 20)
+        ]
+        label.attributedText = NSAttributedString(string: "Trending", attributes: attributes)
+        uiView.addSubview(label)
+        return uiView
     }
 }
 
 // MARK: - UITableViewDelegate
 extension HomeViewController: UITableViewDelegate {
-
+    
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 260
     }
-
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 60
     }
@@ -106,11 +114,11 @@ extension HomeViewController: UITableViewDelegate {
 
 // MARK: - UICollectionViewDataSource
 extension HomeViewController: UICollectionViewDataSource {
-
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return viewModel.imageList.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         guard let slideCell = collectionView.dequeueReusableCell(withReuseIdentifier: "SliderCell", for: indexPath) as? SliderCell else { return CollectionCell() }
         slideCell.viewModel = viewModel.getImageForSlide(atIndexPath: indexPath)
@@ -120,11 +128,11 @@ extension HomeViewController: UICollectionViewDataSource {
 
 // MARK: - UICollectionViewDelegateFlowLayout
 extension HomeViewController: UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: UIScreen.main.bounds.width, height: collectionView.bounds.height)
     }
--- EateryTour/View/Controller/Home/HomeViewModel.swift
+++ EateryTour/View/Controller/Home/HomeViewModel.swift
@@ -15,26 +15,6 @@ final class HomeViewModel: ViewModel {
     var imageList: [UIImage] = [#imageLiteral(resourceName: "slideFood3"), #imageLiteral(resourceName: "slideFood1"), #imageLiteral(resourceName: "slideFood4"), #imageLiteral(resourceName: "slideFood5"), #imageLiteral(resourceName: "slideFood2")]
 
     // MARK: - Public functions
-    func getConfigSection(sectionType: SectionType) -> CustomHeaderViewModel? {
-        switch sectionType {
-        case .trending:
-            return CustomHeaderViewModel(name: "Trending")
-        case .category:
-            return CustomHeaderViewModel(name: "Categories")
-        }
-    }
-
-    func getSectionType(section: Int) -> SectionType {
-        switch section {
-        case 0:
-            return .trending
-        case 1:
-            return .category
-        default:
-            return .category
-        }
-    }
-
     func getImageForSlide(atIndexPath indexPath: IndexPath) -> SlideCellViewModel? {
         return SlideCellViewModel(image: imageList[indexPath.row])
     }
