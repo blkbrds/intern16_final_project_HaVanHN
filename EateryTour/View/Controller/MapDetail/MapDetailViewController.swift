195a53c68dae7bb80ed338d5d79fb892722f6ef6
Ha Van H.N <ha.van@asiantech.vn>
2020-10-01 09:17:50 +0700

    Implement UI

--- EateryTour/View/Controller/MapDetail/MapDetailViewController.swift
+++ EateryTour/View/Controller/MapDetail/MapDetailViewController.swift
@@ -7,15 +7,18 @@
 //
 
 import UIKit
+import MapKit
 
 final class MapDetailViewController: UIViewController {
 
     // MARK: - IBOutlets
+    @IBOutlet private weak var backButton: Button!
+    @IBOutlet private weak var mapView: MKMapView!
 
     // MARK: - Propeties
     var viewModel: MapDetailViewModel? {
         didSet {
-            configUI()
+            updateUI()
         }
     }
 
@@ -24,16 +27,29 @@ final class MapDetailViewController: UIViewController {
     // MARK: - Life cycle
     override func viewDidLoad() {
         super.viewDidLoad()
+        configUI()
     }
     // MARK: - Override functions
-
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        view.frame = CGRect(x: 0, y: 0, width: screenSize.width, height: screenSize.height)
+        view.layoutIfNeeded()
+    }
     // MARK: - Private functions
     private func configUI() {
+        backButton?.tintColor = .white
     }
+
+    private func updateUI() {
+
+    }
+
     // MARK: - Public functions
 
     // MARK: - Objc functions
 
     // MARK: - IBActions
-
+    @IBAction private func backButtonTouchUpInside(_ sender: Button) {
+        navigationController?.popViewController(animated: true)
+    }
 }
